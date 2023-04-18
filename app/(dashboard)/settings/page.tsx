3b589d3011085a0359d6c265aebe1f8bb83feb93
Oscar Gallego <hola@oscargallegoruiz.com>
2023-04-18 13:13:44 +0200

    add try catch logout

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { useRouter } from "next/navigation";
 import Button from "@/components/Button";
-import Card from "@/components/Card";
-import { useEffect, useState } from "react";
 
 export default function Settings() {
   const router = useRouter();
-  const HandleClick = async () => {
-    const response = await fetch("/api/signout", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    });
-    router.push("/");
+  const HandleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/api/signout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      router.push("/");
+    } catch (e) {
+      console.error(e);
+    }
   };
   return (
     <Button onClick={HandleClick} typeof="submit" intent="secondary">
